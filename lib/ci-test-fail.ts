d3589c09e88e3624b8a9c218dc9fb5a4de07d51e
vishal <rautv2005@gmail.com>
2026-02-01 21:25:50 +0530

    Remove CI test file to fix Vercel deployment

--- lib/ci-test-fail.ts
+++
@@ -1,8 +0,0 @@
-// // This file is INTENTIONALLY broken to test the CI workflow.
-// // It has a type error (assigning string to number).
-
-// const brokenMath: number = "this is not a number";
-
-// export function tryToBuild() {
-//   console.log(brokenMath);
-// }
